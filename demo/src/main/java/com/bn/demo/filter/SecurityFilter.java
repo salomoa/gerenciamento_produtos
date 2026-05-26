973925fd31a86568b1c5958505fe10f0da832e8d
salomoa <salomaoantoniobr@gmail.com>
2026-05-25 22:16:42 -0300

    AULA ELIEL 25/05

---
+++ demo/src/main/java/com/bn/demo/filter/SecurityFilter.java
@@ -0,0 +1,30 @@
+package com.bn.demo.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.GET, "/produtos").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/produtos").permitAll()
+                            .requestMatchers(HttpMethod.DELETE, "/produtos/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/produtos/**").permitAll()
+                            .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
